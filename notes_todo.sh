b3c2b9bd38de294a12d8bba0ed149132bff98ad3
Libor Wagner <liborwagner@gmail.com>
2012-08-18 23:14:19 +0200

    Fix command line parsing.

--- notes_todo.sh
+++ notes_todo.sh
@@ -3,38 +3,36 @@ notes_todo() {
 
 
     if [ "$1" = "--complete" ]; then
-        echo "-d"
-        echo "-h"
+        _notes_complete_files
     else
         # Defaults
         INCLUDE_DONE=false
         USAGE="usage: n todo [-dh]"
 
         # Parse command line options
-        while getopts hd OPT; do
-            case "$OPT" in
-                d)
+        args=`getopt dh $*`
+        if [ $? != 0 ]; then echo $USAGE; return 1; fi
+        set -- $args
+        for i; do
+            case "$i" in
+                -d)
                     INCLUDE_DONE=true
                     ;;
-                h)
+                -h)
                     echo $USAGE
                     return 0
-                    ;;
-                \?)
-                    echo $USAGE
-                    return 1
                     ;;
             esac
         done
 
         # Show todo list
         (cd $NOTES_DIR
-            if $INCLUDE_DONE; then
-                grep -i @todo *
-            else
-                grep -i @todo *| \
-                    grep -v @done
-            fi
+        if $INCLUDE_DONE; then
+            grep -i @todo *
+        else
+            grep -i @todo *| \
+                grep -v @done
+        fi
 
         )
     fi
